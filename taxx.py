9ade2327e934b7fee45ba7ef72336a6862b9a59f
compukidsdgp-creator <compukidsdgp@gmail.com>
2026-03-21 16:22:30 +0530

    Update taxx.py

--- taxx.py
+++ taxx.py
@@ -16,7 +16,7 @@ from reportlab.graphics.charts.barcharts import VerticalBarChart
 st.set_page_config(page_title="DNA Tax Optimizer", layout="wide")
 
 # 🔑 SET YOUR GEMINI API KEY
-genai.configure(api_key="AIzaSyA5a698ZqSTdG2_G3SGqJq4_nz2ZDpP8F0")
+genai.configure(api_key="AIzaSyAU3HNcdLH-TJ963xhdkHlMLB7dTxxQ3Yc")
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
