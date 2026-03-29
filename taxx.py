9a0302d482235d658888ad7531094ac5a08963ec
compukids durgapur <compukidsdgp@gmail.com>
2026-03-29 19:12:20 +0530

    Update taxx.py

--- taxx.py
+++ taxx.py
@@ -15,7 +15,7 @@ from reportlab.graphics.charts.barcharts import VerticalBarChart
 
 
 # 🔑 SET YOUR GEMINI API KEY
-genai.configure(api_key="AIzaSyDQ7HSkSUTwg5d3KJcje5_HS775AhEO_ZY")
+genai.configure(api_key="AIzaSyAZZi23agBBGJvV7TgmyUUzRWlyxUQ45eU")
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
